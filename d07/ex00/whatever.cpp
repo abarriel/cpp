954f7672e85cf11b6b4586b2c17389ed2e56b58d
abarriel <abarriel@student.42.fr>
2018-04-05 17:38:10 +0200

    draft

--- d07/ex00/whatever.cpp
+++ d07/ex00/whatever.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+class Awesome {
+
+public:
+
+Awesome( int n ) : _n( n ) {}
+
+bool operator==( Awesome const & rhs ) { return (this->_n == rhs._n); }
+bool operator!=( Awesome const & rhs ) { return (this->_n != rhs._n); }
+bool operator>( Awesome const & rhs ) { return (this->_n > rhs._n); }
+bool operator<( Awesome const & rhs ) { return (this->_n < rhs._n); }
+bool operator>=( Awesome const & rhs ) { return (this->_n >= rhs._n); }
+bool operator<=( Awesome const & rhs ) { return (this->_n <= rhs._n); }
+
+private:
+
+int _n;
+};
 
 template<typename T>
 T const & max(T const & x, T const & y) { return ( (y >= x) ? y : x ); }
+// template<typename T>
+// T  & max(T  & x, T  & y) { return ( (y >= x) ? y : x ); }
 template<typename T>
-T const & min(T const & x, T const & y) { return ( (y <= x) ? y : x ); }
+T const & min(T const & x, T const & y) { return ( (y >= x) ? y : x ); }
+// template<typename T>
+// T  & min(T  & x, T  & y) { return ( (y <= x) ? y : x ); }
 
 template<typename T>
 void swap(T& x, T & y) { 
@@ -13,7 +34,6 @@ void swap(T& x, T & y) {
 	y = x;
 	x = tmp;
 }
-
 
 int main(void) {
 	int i,i1,i2;
@@ -36,7 +56,8 @@ int main(void) {
 	std::cout << "1:" << f1 <<  " - 2:" << f2 << std::endl << std::endl;
 
 	std::cout << "1:" << c1 <<  " - 2:" << c2 << std::endl;
-	swap(c1, c2);
+
+	max(Awesome(45), Awesome(46));
 	std::cout << "1:" << c1 <<  " - 2:" << c2 << std::endl << std::endl;
 
 	i = max(1, 2);
